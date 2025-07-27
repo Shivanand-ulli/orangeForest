4ec2c4611bb78c13122583b4c3d946b22f552c84
Shivanand-ulli <shivanandulli9480@gmail.com>
2025-07-27 20:54:32 +0530

    Updated Card Script

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -5,41 +5,59 @@ using UnityEngine.UI;
 
 public class Card : MonoBehaviour
 {
+    // Front Face Container
     [Header("Front Side")]
     public GameObject frontContainer;
     public Image frontImage;
     public Image frontSign;
 
+    // Back Face Container
     [Header("Back Side")]
     public GameObject backContainer;
     public Image backImage;
     public Image backSign;
 
-    private bool isFliping = false;
-    private bool isMatching = false;
+    // Flags to check
+    private bool isFlipped = false;
+    private bool isMatched = false;
+    
+    private Button button;  
+
+    void Awake()
+    {
+        // Reference of the button
+        button = GetComponent<Button>();
+        if(button != null)
+            button.OnClick.AddListener(OnCardClik);
+    }
 
     void Start()
     {
+        // Initially show backface of the card
         ShowBack();
     }
 
+    // Set the front card face and image
     public void SetCardFace(Sprite bgSprite, Sprite signSprite)
     {
         frontImage.sprite = bgSprite;
         frontSign.sprite = signSprite;
     }
 
+    // Button listener func to the flip card
     public void OnCardClik()
     {
-        if (isMatching || isFliping) return;
+        if (isMatched || isFliped) return;
 
         CardFlip();
         GridManager.Instance.CardRevealed(this);
     }
 
+    /*Call the ShowFront and ShowBack method 
+    according to the isFlipped flag*/
     public void CardFlip()
     {
-        isFliping = !isFliping;
+        isFlipped = !isFlipped;
 
         if (isFliping)
         {
@@ -51,22 +69,33 @@ public class Card : MonoBehaviour
         }
     }
 
+    // Show front face of the card
     public void ShowFront()
     {
         backContainer.SetActive(false);
         frontContainer.SetActive(true);
     }
 
+    // Show back face of the card
     public void ShowBack()
     {
         backContainer.SetActive(true);
         frontContainer.SetActive(false);
     }
 
+    /* Turn off the button interaction and 
+     StartCoroutine after both card matches.*/
     public void CardMatch()
     {
-        isMatching = true;
-        GetComponent<Button>().interactable = false;
+        isMatched = true;
+        button.interactable = false;
+        StartCoroutine(HideMatchCard());
+    }
+
+    // Hide matched card using coroutine
+    private IEnumerator HideMatchCard()
+    {
+        yield return new WaitForSeconds(0.3f);
         backContainer.SetActive(false);
         frontContainer.SetActive(false);
     }
